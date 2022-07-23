2e395cbc51e65d259bce8dbd9de3af1964f4c730
Rafael Moura <rafaelcostamoura@gmail.com>
2022-07-23 10:57:25 -0300

    define global css and fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
+import Head from "../../node_modules/next/head";
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <Head>
+        <title>IG.News | Home</title>
+      </Head>
+      <h1>Home</h1>
+    </>
+  );
 }
