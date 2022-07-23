55cc035af0952a9435fa17976ae2641868c11e63
Rafael Moura <rafaelcostamoura@gmail.com>
2022-07-23 10:57:01 -0300

    define global css and fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
-function MyApp({ Component, pageProps }) {
+import { AppProps } from "../../node_modules/next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
