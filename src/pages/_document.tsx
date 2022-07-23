9e040b8461fb5576bd09a900318fbe9538d9565f
Rafael Moura <rafaelcostamoura@gmail.com>
2022-07-23 11:22:51 -0300

    create header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,6 +16,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&family=Ubuntu:wght@400;700&display=swap"
             rel="stylesheet"
           />
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
         <body>
           <Main />
